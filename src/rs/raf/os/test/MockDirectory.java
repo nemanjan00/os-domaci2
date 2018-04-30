a9dc3a9df6ff5f0874b843a7a089fb1f07e437c8
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-04-30 17:02:25 +0200

    Tests work

--- src/rs/raf/os/test/MockDirectory.java
+++ src/rs/raf/os/test/MockDirectory.java
@@ -5,8 +5,11 @@ import rs.raf.os.dir.DirectoryException;
 import rs.raf.os.disk.Disk;
 import rs.raf.os.fat.FAT16;
 
+import rs.raf.os.disk.DiskUtil;
+
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.Arrays;
 
 import java.util.ArrayList;
 
@@ -26,13 +29,75 @@ public class MockDirectory implements Directory {
 	
 	@Override
 	public boolean writeFile(String name, byte[] data) {
+		if(this.files.containsKey(name)){
+			if(this.getUsableFreeSpace() + (int)Math.ceil((double)this.files.get(name).size * this.fat.getClusterWidth() * this.disk.getSectorSize()) >= data.length){
+				this.deleteFile(name);
+			}
+		}
+
+		if(this.getUsableFreeSpace() < data.length){
+			return false;
+		}
+
+		int cluster = 0;
+
+		int clustersRemaining = (int)Math.ceil((double)data.length / (this.disk.getSectorSize() * this.fat.getClusterWidth()));
+		int clustersWritten = 0;
+
+		int lastSector = 0;
+
+		for(int i = 2; i < this.fat.getClusterCount() + 2; i++){
+			if(this.fat.readCluster(i) == 0){
+				if(lastSector != 0){
+					this.fat.writeCluster(lastSector, i);
+				} else {
+					cluster = i;
+				}
+
+				lastSector = i;
+
+				disk.writeSectors((i - 2) * this.fat.getClusterWidth(), this.fat.getClusterWidth(), DiskUtil.slice(data, clustersWritten * this.fat.getClusterWidth() * this.disk.getSectorSize(), this.fat.getClusterWidth() * this.disk.getSectorSize()));
+
+				clustersRemaining--;
+				clustersWritten++;
+
+				if(clustersRemaining == 0){
+					this.fat.writeCluster(i, this.fat.getEndOfChain());
+
+					break;
+				}
+			}
+		};
+
+		this.files.put(name, new File(name, data.length, cluster));
+
 		return true;
 	}
 
 	@Override
 	public byte[] readFile(String name) throws DirectoryException {
 		if(this.files.containsKey(name)){
-			return new byte[1];
+			File file = this.files.get(name);
+
+			int cluster = file.cluster;
+
+			byte[] data = {};
+			byte[] newData;
+			byte[] newDataConcatenated;
+
+			do {
+				newData = this.disk.readSectors((cluster - 2) * this.fat.getClusterWidth(), this.fat.getClusterWidth());
+
+				newDataConcatenated = Arrays.copyOf(data, data.length + newData.length);
+				System.arraycopy(newData, 0, newDataConcatenated, data.length, newData.length);
+				data = newDataConcatenated;
+
+				cluster = this.fat.readCluster(cluster);
+			} while(cluster != this.fat.getEndOfChain());
+
+			data = DiskUtil.slice(data, 0, file.size);
+
+			return data;
 		} else {
 			throw new DirectoryException("File not found! ");
 		}
@@ -44,10 +109,14 @@ public class MockDirectory implements Directory {
 			File file = this.files.get(name);
 
 			int cluster = file.cluster;
+			int newCluster;
 
 			do {
-				cluster = this.fat.readCluster(cluster);
+				newCluster = this.fat.readCluster(cluster);
+
 				this.fat.writeCluster(cluster, 0);
+
+				cluster = newCluster;
 			} while(cluster != this.fat.getEndOfChain());
 
 			this.files.remove(name);
@@ -93,7 +162,7 @@ public class MockDirectory implements Directory {
 		for(Map.Entry<String, File> entry: this.files.entrySet()) {
 			File file = entry.getValue();
 
-			usableTotalSpace -= Math.ceil(file.size / clusterSize) * clusterSize;
+			usableTotalSpace -= Math.ceil((double)file.size / clusterSize) * clusterSize;
 		}
 
 		return usableTotalSpace;
